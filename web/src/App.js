0a04d00d75818c917ef5bb969e486630bf59196f
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-01-15 18:00:13 -0300

    finalizando frontend web

--- web/src/App.js
+++ web/src/App.js
@@ -1,128 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api'
 
 import './global.css'
 import './App.css'
 import './sidebar.css'
 import './main.css'
 
+import DevForm from './components/DevForm'
+import DevItem from './components/DevItem'
+
 function App() {
-  const [latitude, setLatitude] = useState('')
-  const [longitude, setLongitude] = useState('')
-  const [github_username, setGithub_username] = useState('')
-  const [techs, setTechs] = useState('')
+  const [devs, setDevs] = useState([])
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords
-
-        setLatitude(latitude)
-        setLongitude(longitude)
-      },
-      (err) => {
-        console.log(err)
-      }, {
-      timeout: 30000,
-    })
-
+    async function loadDevs() {
+      const response = await api.get('/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
   }, [])
 
-  async function handleAddDev(e) {
-    e.preventDefault();
-
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+    setDevs([...devs, response.data])
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required value={github_username} onChange={e => setGithub_username(e.target.value)}></input>
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required value={techs} onChange={e => setTechs(e.target.value)}></input>
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              ></input>
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              ></input>
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/46037459?s=460&v=4" alt="Jefferson Brito"></img>
-              <div className="user-info">
-                <strong>Jefferson Brito</strong>
-                <span> ReactJS , React Native, Node.js</span>
-              </div>
-            </header>
-            <p>estudante de analise e desenvolvimento de sistemas, com meta de se tornar um desenvolvedor full-stack</p>
-            <a href="https://github.com/jeffersontk"> Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/46037459?s=460&v=4" alt="Jefferson Brito"></img>
-              <div className="user-info">
-                <strong>Jefferson Brito</strong>
-                <span> ReactJS , React Native, Node.js</span>
-              </div>
-            </header>
-            <p>estudante de analise e desenvolvimento de sistemas, com meta de se tornar um desenvolvedor full-stack</p>
-            <a href="https://github.com/jeffersontk"> Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/46037459?s=460&v=4" alt="Jefferson Brito"></img>
-              <div className="user-info">
-                <strong>Jefferson Brito</strong>
-                <span> ReactJS , React Native, Node.js</span>
-              </div>
-            </header>
-            <p>estudante de analise e desenvolvimento de sistemas, com meta de se tornar um desenvolvedor full-stack</p>
-            <a href="https://github.com/jeffersontk"> Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/46037459?s=460&v=4" alt="Jefferson Brito"></img>
-              <div className="user-info">
-                <strong>Jefferson Brito</strong>
-                <span> ReactJS , React Native, Node.js</span>
-              </div>
-            </header>
-            <p>estudante de analise e desenvolvimento de sistemas, com meta de se tornar um desenvolvedor full-stack</p>
-            <a href="https://github.com/jeffersontk"> Acessar perfil no Github</a>
-          </li>
-
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
